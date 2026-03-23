88296c85ace1b022de9bffc6e8826c47c400c3ab
ram <identikited@gmail.com>
2026-03-23 06:52:56 -0500

    Bad idea D :

--- modules/hosts/store.nix
+++ modules/hosts/store.nix
@@ -7,9 +7,6 @@
     # Enable flakes
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-    # Enable dynamic executables
-    programs.nix-ld.enable = true;
-
     # Automatic storage optimization
     nix.optimise.automatic = true;
     nix.optimise.dates = [ "07:00" ];
