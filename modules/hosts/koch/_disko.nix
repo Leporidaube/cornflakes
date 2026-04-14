e8b38399f62578b918fb657e1efba07acebf6bf2
ram <identikited@gmail.com>
2026-04-13 22:56:53 -0500

    Added encryption to disko

--- modules/hosts/koch/_disko.nix
+++ modules/hosts/koch/_disko.nix
@@ -1,39 +1,43 @@
-# modules/hosts/koch/_disko.nix
+  # modules/hosts/koch/_disko.nix
 
 { ... }:
-{
-  disko.devices = {
-    disk.main = {
-      type = "disk";
-      device = "/dev/nvme0n1";
-      content = {
-        type = "gpt";
-        partitions = {
-          ESP = {
-            size = "1G";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
+  {
+    disko.devices = {
+      disk.main = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
             };
-          };
-          swap = {
-            size = "32G";
-            content = {
-              type = "swap";
+            swap = {
+              size = "32G";
+              content = {
+                type = "swap";
+              };
             };
-          };
-          root = {
-            size = "1430G";  # ~1.76TB - 32G - 1G - 300G unallocated
-            content = {
-              type = "filesystem";
-              format = "btrfs";
-              mountpoint = "/";
+            root = {
+              size = "1430G";
+              content = {
+                type = "luks";
+                name = "cryptroot";
+                content = {
+                  type = "filesystem";
+                  format = "btrfs";
+                  mountpoint = "/";
+                };
+              };
             };
           };
         };
       };
     };
-  };
-}
+  }
