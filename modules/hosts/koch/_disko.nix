f97d3fcc1de28ee6a25cd646b21ecf19aab9f8ee
ram <identikited@gmail.com>
2026-04-16 16:01:57 -0500

    Fix: Syntax error

--- modules/hosts/koch/_disko.nix
+++ modules/hosts/koch/_disko.nix
@@ -41,20 +41,19 @@
                 extraArgs = [ "-f" ];
 
                 subvolumes = {
-
                   "/root" = {
                     mountpoint = "/";
-                    mountOptions = [ "compress=zstd", "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" ];
                   };
 
                   "/home" = {
                     mountpoint = "/home";
-                    mountOptions = [ "compress=zstd", "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" ];
                   };
 
                   "/nix" = {
                     mountpoint = "/nix";
-                    mountOptions = [ "compress=zstd", "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" ];
                   };
                 };
               };
